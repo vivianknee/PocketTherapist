01f9fa86aaaed5c9cd0166475610b34d68969062
tanishapatil1234 <tanishapatil07@gmail.com>
2023-10-26 22:10:41 -0700

    captureEmotion function

--- cscript.js
+++ cscript.js
@@ -1,5 +1,22 @@
 const video = document.getElementById('video')
+let detections; 
+let detectionComplete = false;
 
+// Loading all the models ** github downloads? 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    // Registers facial features
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    // Box around face
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    // Emotion detector
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo())
+
+
+
+
+// Initialize Camer (call face-api.min before this file to render model before the camera )
 function startVideo() {
     navigator.getUserMedia(
         { video: {} }, 
@@ -8,4 +25,57 @@ function startVideo() {
     )
 }
 
-startVideo()
+
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+    detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections, 
+    displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+    detectionComplete = true;
+
+    }, 100) // repetiion of 100 ms
+  })
+
+// Function to capture and display the highest emotion
+// Function to capture and display the raw response
+// Function to capture and display the raw response
+document.getElementById('captureEmotionButton').addEventListener('click', captureEmotion);
+
+function captureEmotion() {
+    if (detectionComplete) {
+        if (detections && detections.length > 0) {
+            const expressions = detections[0].expressions; // Access expressions from the first detected face
+            let maxEmotion = 'neutral'; // Default emotion
+            let maxScore = 0;
+
+            for (const emotion in expressions) {
+                const score = expressions[emotion];
+                if (score > maxScore) {
+                    maxScore = score;
+                    maxEmotion = emotion;
+                }
+            }
+
+            // Display the highest emotion
+            alert(`Highest Emotion: ${maxEmotion}`);
+        } else {
+            alert('No face detected.');
+        }
+    } else {
+        alert('Detection is not complete yet. Please wait.');
+    }
+}
+
+
+
